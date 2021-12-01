82fa19185417f79f8f6dfb88522349d4f4afff84
Brian <briancdang@gmail.com>
2021-12-01 05:38:17 +0000

    Slight ergonomic improvement

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import time
 import day01
 
 p1, p2 = 'p1', 'p2'
-def run(module, p, input):
+def run(module, p):
   start = time.time()
 
   print(f"\n{module.__name__} {p}:")
-  getattr(module, p)(input)
+  getattr(module, p)(f"inputs/{module.__name__}.txt")
 
   end = time.time()
   print(f"(t={end - start})")
 
 day01.test()
-run(day01, p1, "inputs/day01.txt")
-run(day01, p2, "inputs/day01.txt")
+run(day01, p1)
+run(day01, p2)
