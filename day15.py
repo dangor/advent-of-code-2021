b418d119153b14d5900409a97f0687e9cd2e3597
Brian <briancdang@gmail.com>
2021-12-15 06:58:50 +0000

    Day 15, simplified dijkstra, but realized my real problem was not going up or left

--- day15.py
+++ day15.py
@@ -1,26 +1,29 @@
-from a_star import a_star
+from heapq import heappush, heappop
 
 def p1(input):
   file = open(input)
-  lines = list(list(map(int, list(x.strip('\n')))) for x in file.readlines())
+  board = list(list(map(int, list(x.strip('\n')))) for x in file.readlines())
   file.close()
 
-  start = (0, 0)
-  goal = (len(lines) - 1, len(lines[0]) - 1)
+  current = (0, 0)
+  goal = (len(board) - 1, len(board[0]) - 1)
+  visited = set([current])
+  h = []
+  heappush(h, (0, current))
 
-  def neighbors(coord):
-    neighbors = []
-    if coord[0] < goal[0]:
-      neighbors.append((coord[0] + 1, coord[1]))
-    if coord[1] < goal[1]:
-      neighbors.append((coord[0], coord[1] + 1))
-    return neighbors
+  while len(h) > 0:
+    (risk, lowest) = heappop(h)
+    offsets = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for offset in offsets:
+      x = offset[0] + lowest[0]
+      y = offset[1] + lowest[1]
+      if x >= len(board) or y >= len(board[0]) or x < 0 or y < 0:
+        continue
 
-  path = a_star(lines, start, goal, neighbors)
-
-  path.pop(0) # remove start
-  risk = 0
-  for coord in path:
-    risk += lines[coord[0]][coord[1]]
-
-  return risk
+      new_risk = risk + board[x][y]
+      if (x, y) == goal:
+        return new_risk
+      if (x, y) in visited:
+        continue
+      visited.add((x, y))
+      heappush(h, (new_risk, (x, y)))
