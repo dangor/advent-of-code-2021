fc0aa0e38d9455bf053b910f4ca3243975ef5a26
Brian <briancdang@gmail.com>
2021-12-20 23:20:18 +0000

    Day 18 p1

--- day18.py
+++ day18.py
@@ -4,12 +4,30 @@ class Node(object):
   def __init__(self, left = None, right = None, parent = None):
     self.left = left
     self.right = right
+    self.set_left(left)
+    self.set_right(right)
     self.parent = parent
     self.neighbor_left = None
     self.neighbor_right = None
 
   def __str__(self):
     return f"[{self.left},{self.right}]"
+
+  def set_right(self, right):
+    self.right = right
+    if type(self.left) == Node:
+      self.left.neighbor_right = right
+    if type(right) == Node:
+      self.right.neighbor_left = self.left
+      self.right.parent = self
+
+  def set_left(self, left):
+    self.left = left
+    if type(self.right) == Node:
+      self.right.neighbor_left = left
+    if type(left) == Node:
+      self.left.neighbor_right = self.right
+      self.left.parent = self
 
   def explode(self, depth = 0):
     if type(self.left) == Node and self.left.explode(depth + 1):
@@ -22,40 +40,36 @@ class Node(object):
         current = current.parent
       if current and type(current.neighbor_left) != Node:
         current = current.parent
-        print(f"exploding left {self.left} -> {current.left}")
-        current.left += self.left
+        #print(f"exploding left {self.left} -> {current.left}")
+        current.set_left(current.left + self.left)
       elif current:
         current = current.neighbor_left
         while current and type(current.right) == Node:
           current = current.right
         if current:
-          print(f"exploding left {self.left} -> {current.right}")
-          current.right += self.left
+          #print(f"exploding left {self.left} -> {current.right}")
+          current.set_right(current.right + self.left)
 
       current = self
       while current and current.neighbor_right is None:
         current = current.parent
       if current and type(current.neighbor_right) != Node:
         current = current.parent
-        print(f"exploding right {self.right} -> {current.right}")
-        current.right += self.right
+        #print(f"exploding right {self.right} -> {current.right}")
+        current.set_right(current.right + self.right)
       elif current:
         current = current.neighbor_right
         while current and type(current.left) == Node:
           current = current.left
         if current:
-          print(f"exploding right {self.right} -> {current.left}")
-          current.left += self.right
+          #print(f"exploding right {self.right} -> {current.left}")
+          current.set_left(current.left + self.right)
       
       if self.parent.left == self:
-        self.parent.left = 0
-        if type(self.parent.right) == Node:
-          self.parent.right.neighbor_left = 0
+        self.parent.set_left(0)
 
       if self.parent.right == self:
-        self.parent.right = 0
-        if type(self.parent.left) == Node:
-          self.parent.left.neighbor_right = 0
+        self.parent.set_right(0)
 
       return True
 
@@ -64,21 +78,15 @@ class Node(object):
   def split(self):
     if type(self.left) == Node and self.left.split():
       return True
+    elif type(self.left) != Node and self.left >= 10:
+      #print(f"splitting {self.left}")
+      self.set_left(Node(self.left // 2, ceil(self.left / 2), self))
+      return True
     elif type(self.right) == Node and self.right.split():
       return True
-    elif type(self.left) != Node and self.left >= 10:
-      print(f"splitting {self.left}")
-      self.left = Node(self.left // 2, ceil(self.left / 2), self)
-      self.left.neighbor_right = self.right
-      if type(self.right) == Node:
-        self.right.neighbor_left = self.left
-      return True
     elif type(self.right) != Node and self.right >= 10:
-      print(f"splitting {self.right}")
-      self.right = Node(self.right // 2, ceil(self.right / 2), self)
-      self.right.neighbor_left = self.left
-      if type(self.left) == Node:
-        self.left.neighbor_right = self.right
+      #print(f"splitting {self.right}")
+      self.set_right(Node(self.right // 2, ceil(self.right / 2), self))
       return True
 
     return False
@@ -91,13 +99,8 @@ def p1(input):
   current = convert_to_btree(eval(lines.pop(0)))
   while lines:
     current = Node(current, convert_to_btree(eval(lines.pop(0))))
-    print(current)
-    current.left.parent = current
-    current.right.parent = current
-    current.left.neighbor_right = current.right
-    current.right.neighbor_left = current.left
+    #print(current)
     reduce_snail(current)
-    break
 
   return magnitude(current)
 
@@ -106,12 +109,8 @@ def convert_to_btree(snail, parent = None):
     return snail
   
   new = Node(parent = parent)
-  new.left = convert_to_btree(snail[0], new)
-  new.right = convert_to_btree(snail[1], new)
-  if type(new.left) == Node:
-    new.left.neighbor_right = new.right
-  if type(new.right) == Node:
-    new.right.neighbor_left = new.left
+  new.set_left(convert_to_btree(snail[0], new))
+  new.set_right(convert_to_btree(snail[1], new))
   return new
 
 def reduce_snail(snail):
@@ -120,14 +119,10 @@ def reduce_snail(snail):
       exploded = snail.explode()
       if not exploded:
         break
-      else:
-        print(snail)
 
     did_split = snail.split()
     if not did_split:
       break
-    else:
-      print(snail)
 
   return snail
 
